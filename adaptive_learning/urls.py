dffca5797794577151170a90117ed4b0bd845849
Edward Cheng <51186528+EC061@users.noreply.github.com>
2026-02-20 11:28:49 +0000

    feat: add URL pattern for favicon.ico redirect to static file.

--- adaptive_learning/urls.py
+++ adaptive_learning/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('my_app.urls')),
+    path("admin/", admin.site.urls),
+    path(
+        "favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)
+    ),
+    path("", include("my_app.urls")),
 ]
